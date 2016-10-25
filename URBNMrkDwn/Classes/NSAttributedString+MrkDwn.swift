a8b31848fe9f1978c9469ca44a40f01be4073173
Jason Grandelli <jasongrandelli@gmail.com>
2016-10-25 13:01:21 -0400

    Add Attributed String convenience

---
+++ URBNMrkDwn/Classes/NSAttributedString+MrkDwn.swift
@@ -0,0 +1,26 @@
+//
+//  NSAttributedString+MrkDwn.swift
+//  Pods
+//
+//  Created by Jason Grandelli on 10/25/16.
+//
+//
+
+import Foundation
+import UIKit
+
+public extension NSAttributedString {
+    public convenience init(htmlString: String) throws {
+        guard let htmlData = htmlString.data(using: .utf8) else { throw MrkDwnRenderErrors.htmlDataConversionError }
+        
+        let opt: [String: Any] = [NSDocumentTypeDocumentAttribute: NSHTMLTextDocumentType, NSCharacterEncodingDocumentAttribute: String.Encoding.utf8]
+        
+        try self.init(data: htmlData, options: opt, documentAttributes: nil)
+    }
+    
+    public convenience init(markdownString: String, options: MrkDwnOptions = .default) throws {
+        let htmlString = try MrkDwnRenderers.renderHTMLFromMarkdown(markdownString, options: options)
+
+        try self.init(htmlString: htmlString)
+    }
+}
