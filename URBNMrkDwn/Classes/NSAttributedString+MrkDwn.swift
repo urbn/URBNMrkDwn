960c403f3f880bfce9be51ab6a367c2038012a05
Jason Grandelli <jasongrandelli@gmail.com>
2016-10-25 13:54:15 -0400

    Add tests for String & AttributedString convenience

--- URBNMrkDwn/Classes/NSAttributedString+MrkDwn.swift
+++ URBNMrkDwn/Classes/NSAttributedString+MrkDwn.swift
@@ -13,7 +13,7 @@ public extension NSAttributedString {
     public convenience init(htmlString: String) throws {
         guard let htmlData = htmlString.data(using: .utf8) else { throw MrkDwnRenderErrors.htmlDataConversionError }
         
-        let opt: [String: Any] = [NSDocumentTypeDocumentAttribute: NSHTMLTextDocumentType, NSCharacterEncodingDocumentAttribute: String.Encoding.utf8]
+        let opt: [String: Any] = [NSDocumentTypeDocumentAttribute: NSHTMLTextDocumentType, NSCharacterEncodingDocumentAttribute: NSNumber(value: String.Encoding.utf8.rawValue)]
         
         try self.init(data: htmlData, options: opt, documentAttributes: nil)
     }
