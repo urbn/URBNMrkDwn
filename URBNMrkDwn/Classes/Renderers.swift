8b0736c9da0e8fa17e53f37bd3755e0cd50e36f7
Jason Grandelli <jasongrandelli@gmail.com>
2016-10-25 15:33:11 -0400

    Documentation

--- URBNMrkDwn/Classes/Renderers.swift
+++ URBNMrkDwn/Classes/Renderers.swift
@@ -10,6 +10,17 @@ import Foundation
 import cmark
 
 public struct MrkDwnRenderers {
+    /**
+     Converts a Markdown string to an AST.
+     
+     - parameter markdown: String containing Markdown
+     
+     - parameter options: `MrkDwnOptions` defaults to `MrkDwnOptions.default`. Options for rendering markdown to AST.
+     
+     - throws: `MrkDwnRenderErrors.toASTError`
+     
+     - returns: An OpaquePointer for the AST
+     */
     public static func renderASTFromMarkdown(_ markdown: String, options: MrkDwnOptions = .default) throws -> OpaquePointer {
         var node: OpaquePointer?
         
@@ -23,14 +34,36 @@ public struct MrkDwnRenderers {
         return ast
     }
     
+    /**
+     Converts an AST to HTML.
+     
+     - parameter ast: OpaquePoint to an AST
+     
+     - parameter options: `MrkDwnOptions` defaults to `MrkDwnOptions.default`. Options for rendering markdown to AST & HTML.
+     
+     - throws: `MrkDwnRenderErrors`
+     
+     - returns: An HTML String
+     */
     public static func renderHTMLFromAST(_ ast: OpaquePointer, options: MrkDwnOptions = .default) throws -> String {
-        guard let cString = cmark_render_html(ast, options.rawValue) else { throw MrkDwnRenderErrors.toASTError }
+        guard let cString = cmark_render_html(ast, options.rawValue) else { throw MrkDwnRenderErrors.astRenderingError }
         defer { free(cString) }
-        guard let string = String(cString: cString, encoding: String.Encoding.utf8) else { throw MrkDwnRenderErrors.toASTError }
+        guard let string = String(cString: cString, encoding: String.Encoding.utf8) else { throw MrkDwnRenderErrors.astRenderingError }
         
         return string
     }
     
+    /**
+     Converts a Markdown string to an AST. Calls through to `renderASTFromMarkdown(_:, options:)` & `renderHTMLFromAST(_:, options:)`
+     
+     - parameter markdown: String containing Markdown
+     
+     - parameter options: `MrkDwnOptions` defaults to `MrkDwnOptions.default`. Options for rendering markdown to HTML.
+     
+     - throws: `MrkDwnRenderErrors`
+     
+     - returns: An HTML String
+     */
     public static func renderHTMLFromMarkdown(_ markdown: String, options: MrkDwnOptions = .default) throws -> String {
         let ast = try MrkDwnRenderers.renderASTFromMarkdown(markdown, options: options)
         defer { cmark_node_free(ast) }
